b771ca17b3fbd642b70964398719965bda92f8da
xpieemma <7chrispeter@gmail.com>
2026-02-10 05:28:00 -0500

    a new feature

--- src/components/UserProfileCard/UserProfileCard.tsx
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -1,25 +1,30 @@
 import type { UserProfileCardProps } from "../../types";
 
 function UserProfileCard({user , showEmail, showRole, onEdit, children }:UserProfileCardProps){
+ if (!user) return <p> loading ... </p>;
+    const {id = "No ID", name = "No Name", email, role, avatarUrl} = user;
 
-    
     return (
-        <>
+    
             <div className="flex flex-row gap-3.5 border border-gray-300 m-3.5">
-                {user.avatarUrl && <div><img src={user.avatarUrl} /></div>}
-                <div className="">
-                    <p>User ID: {user.id}</p>
-                    <p>User Name: {user.name}</p>
-                    {showEmail && (<p>Email: {user.email}</p>)}
-                    {showRole && (<p>Role: {user.role}</p>)}
-                    {onEdit && (<button onClick={()=>onEdit(user.id)} className="border border-gray-300 p-3.5 pt-1.5 pb-1.5">Edit</button>)}
+                {avatarUrl ? (<img src={avatarUrl} alt={`${name}'s avatar`} />) :
+                
+                  (<div className = "w-16 h-16 bg-gray-200 rounded-full flex items-center justify-center" > No Avatar</div>)}
+                <div className="flex flex-col gap-1">
+                    <p> <strong> User ID:</strong> {id}</p>
+                    <p><strong>User Name:</strong> {name}</p>
+                    {showEmail && ( email ? (<p><strong>Email:</strong> {email}</p>):
+                    (<p className="text-red-500"> Email not provided</p>) )}
+                    {showRole && (role ?  (<p><strong>Role:</strong> {role}</p>) :
+                    (<p className="text-red-500">Role not provided</p>))}
+                    {onEdit && (<button onClick={()=>onEdit(id)} className="border border-gray-300 p-3.5 pt-1.5 pb-1.5">Edit</button>)}
                     <div>
                         {children}
                     </div>
                 </div>
             </div>
-        </>
-    )
+        
+    );
 }
 
 export default UserProfileCard;
